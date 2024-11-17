0fafc9ad1c825fd1e8c0c4ef777245375685e401
Filat Astakhov <ph1l74rus@gmail.com>
2024-11-17 17:52:09 +0300

    Added palletes

--- src/palletes.js
+++ src/palletes.js
@@ -1,10 +1,13 @@
 export const PALLETES = {
   DEFAULT: ["#621717", "#232222", "#D7D6D6", "#524343", "#4F3232"],
-  MEDIUM: [],
-  SUMMERTIME: [],
-  BIRDS: [],
-  DISNEY: [],
-  SPRING2: ["#7BC56F", "#2980F2", "#CF0A62", "#84868F", "#A1779B"],
+  MEDIUM: ["#621717", "#232222", "#D7D6D6", "#524343", "#4F3232"],
+  PURPLE_HAZE: ['#862db1', '#5a2bb9', '#a729a5', '#ce2898', '#644bd1'],
+  SUMMERTIME: ['#cf564c', '#bd322f', '#c95b2a', '#da8a35', '#e17a68'],
+  BIRDS: ['#4cc48e', '#2eada5', '#23b685', '#36c568', '#2f9bc9'],
+  DISNEY: ['#603fa8', '#5d69b2', '#7292c7', '#2d5891', '#9bc5e5'],
   SPRING: ["#AFA2B3", "#A185A6", "#6085C6", "#72A08F", "#D26A8C"],
-  AUTUMN: [],
+  SPRING2: ['#a9718c', '#8782a7', '#5489cc', '#bc4070', '#70a093'],
+  SPRING3: ["#7BC56F", "#2980F2", "#CF0A62", "#84868F", "#A1779B"],
+  AUTUMN: ['#ba985a', '#a8bf5a', '#c9775c', '#d55c70', '#aea23e'],
+  REMEMBERANCE: ['#bcb4b9', '#bf8f90', '#c5cbd0', '#a29399', '#8b7b82']
 };
